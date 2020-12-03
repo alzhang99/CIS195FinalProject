1a2d87125c73a52154cd99241a59bb8ece3d34da
alzhang99 <54654911+alzhang99@users.noreply.github.com>
2020-12-03 14:10:43 -0500

    tabs set up

---
+++ ReadAway/ReadAway/CurrentBooksViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentBooksViewController.swift
+//  ReadAway
+//
+//  Created by Ally Zhang on 12/2/20.
+//
+
+import UIKit
+
+class CurrentBooksViewController: NSObject {
+
+}
---
+++ ReadAway/ReadAway/PreviousBooksViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PreviousBooksViewController.swift
+//  ReadAway
+//
+//  Created by Ally Zhang on 12/2/20.
+//
+
+import UIKit
+
+class PreviousBooksViewController: NSObject {
+
+}
